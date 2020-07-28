b984c0fb725835cf6259db6d220a39d4d9b22a63
PrismSpirit <yeolkeut@gmail.com>
2020-07-29 00:25:42 +0900

    update tutorial1 scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -73,7 +73,7 @@ public class Player : MonoBehaviour
             RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
             if (rayHit.collider != null)
             {
-                if (rayHit.distance < 0.55f)
+                if (rayHit.distance < 1.2f)
                     anim.SetBool("IsJumping", false);
             }
         }
