a1d1cf614841f122cef5d0dd3dd187bce9105370
sohn1029 <sohn1029@gmail.com>
2020-07-28 23:45:59 +0900

    create player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+    public float jumpPower;
+    public float maxSpeed;
+    public float reduceSpeed;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //jump
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("IsJumping"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("IsJumping", true);
+        }
+        //break when button up
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * reduceSpeed, rigid.velocity.y);
+        }
+        //flip player
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+        //set,reset walk animation
+        if (Mathf.Abs(rigid.velocity.x) < 0.3f)
+        {
+            anim.SetBool("IsWalking", false);
+        }
+        else
+        {
+            anim.SetBool("IsWalking", true);
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        //move(left,right)
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        //maxSpeed control
+        if (rigid.velocity.x > maxSpeed)
+        {
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        }
+
+        else if (rigid.velocity.x < maxSpeed * (-1))
+        {
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+        }
+
+        //when falling
+        //set layer as "Platform"
+        Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+        if (rigid.velocity.y < 0)
+        {
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.9f)
+                    anim.SetBool("IsJumping", false);
+            }
+        }
+        Debug.Log(anim.GetBool("IsJumping"));
+        Debug.Log(anim.GetBool("IsWalking"));
+    }
+}
