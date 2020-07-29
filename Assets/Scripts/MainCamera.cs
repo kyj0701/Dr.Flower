6cd56f9187f941eada96460dda08a55ad34b7303
sohn1029 <sohn1029@gmail.com>
2020-07-29 16:31:46 +0900

    create camera_move

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public Camera m_OrthographicCamera;
+    public float camera_size = 4;
+    public float Camera_z = -10;
+    Transform playerTransform;
+    Vector3 Offset;
+    
+    void Awake()
+    {
+        Camera.main.orthographicSize = camera_size;
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        Offset = playerTransform.position;
+        Offset.z = Camera_z;
+    }
+
+    void LateUpdate()
+    {
+        Offset = playerTransform.position;
+        Offset.z = Camera_z;
+        transform.position = Offset;
+    }
+}
