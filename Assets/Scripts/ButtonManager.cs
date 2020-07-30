b764a9c9c30ca0ea5b4d629e753763a2cc99dd28
kyj0701 <yejun283@gmail.com>
2020-07-31 00:06:03 +0900

    Add GameJamStage

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -35,6 +35,11 @@ public class ButtonManager : MonoBehaviour
         SceneManager.LoadScene("Stage0");
         clearStateDic = new Dictionary<string, bool>();
     }
+    public void GameJam()
+    {
+        SceneManager.LoadScene("GameJamStage");
+        clearStateDic = new Dictionary<string, bool>();
+    }
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
@@ -46,22 +51,27 @@ public class ButtonManager : MonoBehaviour
     public void SelectStage1_1()
     {
         SceneManager.LoadScene("Stage1-1");
+        TimeReset();
     }
     public void SelectStage1_2()
     {
         SceneManager.LoadScene("Stage1-2");
+        TimeReset();
     }
     public void SelectStage1_3()
     {
         SceneManager.LoadScene("Stage1-3");
+        TimeReset();
     }
     public void SelectStage1_4()
     {
         SceneManager.LoadScene("Stage1-4");
+        TimeReset();
     }
     public void SelectStage1_5()
     {
         SceneManager.LoadScene("Stage1-5");
+        TimeReset();
     }
     public void SelectStage2()
     {
@@ -70,22 +80,27 @@ public class ButtonManager : MonoBehaviour
     public void SelectStage2_1()
     {
         SceneManager.LoadScene("Stage2-1");
+        TimeReset();
     }
     public void SelectStage2_2()
     {
         SceneManager.LoadScene("Stage2-2");
+        TimeReset();
     }
     public void SelectStage2_3()
     {
         SceneManager.LoadScene("Stage2-3");
+        TimeReset();
     }
     public void SelectStage2_4()
     {
         SceneManager.LoadScene("Stage2-4");
+        TimeReset();
     }
     public void SelectStage2_5()
     {
         SceneManager.LoadScene("Stage2-5");
+        TimeReset();
     }
     public void MainMenu()
     {
