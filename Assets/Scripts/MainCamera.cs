abe75ba3b22a1a321ca98e1a1652a28b7e387048
PrismSpirit <yeolkeut@gmail.com>
2020-07-31 16:30:10 +0900

    add rain effect

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -7,7 +7,11 @@ public class MainCamera : MonoBehaviour
     public float camera_size = 4;
     public float Camera_z = -10;
     Transform playerTransform;
+    Transform rainTransform;
     Vector3 Offset;
+    Vector3 rainpos;
+    public ParticleSystem rain;
+    public bool israin;
     
     void Awake()
     {
@@ -16,11 +20,25 @@ public class MainCamera : MonoBehaviour
         Offset = playerTransform.position;
         Offset.z = Camera_z;
         Offset.y += 1;
+        rainTransform = playerTransform;
+    }
+
+    void Update()
+    {
+        if (israin)
+        {    
+            rain.Play();
+            rainpos = rainTransform.position;
+            rainpos.x -= 4;
+            rainpos.y += 10;
+            rain.transform.position = rainpos;
+        }
+        else
+            rain.Stop();
     }
 
     void LateUpdate()
     {
-        
         Offset = playerTransform.position;
         Offset.y += 1;
         Offset.z = Camera_z;
